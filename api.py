9bb15d58e75d34765b973715176baf4ffae3a7b8
Sanjay <sanjaysusi29@gmail.com>
2024-03-21 18:02:57 +0530

    change cuda visibility

--- api.py
+++ api.py
@@ -1,9 +1,31 @@
+import os
+
+# Save the original CUDA_VISIBLE_DEVICES
+original_cuda_visible_devices = os.environ.get('CUDA_VISIBLE_DEVICES')
+
+# Set CUDA_VISIBLE_DEVICES to -1
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+
+# Import TensorFlow
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import BatchNormalization
+# Reset CUDA_VISIBLE_DEVICES to its original value
+if original_cuda_visible_devices is not None:
+    os.environ['CUDA_VISIBLE_DEVICES'] = original_cuda_visible_devices
+else:
+    del os.environ['CUDA_VISIBLE_DEVICES']
+
 import torch
+import numpy as np
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import time
-import utils
 import uvicorn
-import os
 from fastapi import FastAPI, Body, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 import io
@@ -11,14 +33,103 @@ import threading
 import re
 import ngrok
 from PIL import Image
-original_cuda_visible_devices = os.environ.get('CUDA_VISIBLE_DEVICES')
-os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
-import tensorflow as tf
 
-if original_cuda_visible_devices is not None:
-    os.environ['CUDA_VISIBLE_DEVICES'] = original_cuda_visible_devices
-else:
-    del os.environ['CUDA_VISIBLE_DEVICES']
+
+def load_model_alexnet():
+
+    classifier = Sequential()
+
+    classifier.add(Convolution2D(96, 11, strides = (4, 4), padding = 'valid', input_shape=(224, 224, 3), activation = 'relu'))
+
+    classifier.add(MaxPooling2D(pool_size = (2, 2), strides = (2, 2), padding = 'valid'))
+    classifier.add(BatchNormalization())
+
+    classifier.add(Convolution2D(256, 11, strides = (1, 1), padding='valid', activation = 'relu'))
+
+    classifier.add(MaxPooling2D(pool_size = (2, 2), strides = (2, 2), padding='valid'))
+    classifier.add(BatchNormalization())
+
+    # Convolution Step 3
+    classifier.add(Convolution2D(384, 3, strides = (1, 1), padding='valid', activation = 'relu'))
+    classifier.add(BatchNormalization())
+
+    classifier.add(Convolution2D(384, 3, strides = (1, 1), padding='valid', activation = 'relu'))
+    classifier.add(BatchNormalization())
+
+    classifier.add(Convolution2D(256, 3, strides=(1,1), padding='valid', activation = 'relu'))
+
+    classifier.add(MaxPooling2D(pool_size = (2, 2), strides = (2, 2), padding = 'valid'))
+    classifier.add(BatchNormalization())
+
+    classifier.add(Flatten())
+
+    classifier.add(Dense(units = 4096, activation = 'relu'))
+    classifier.add(Dropout(0.4))
+    classifier.add(BatchNormalization())
+    classifier.add(Dense(units = 4096, activation = 'relu'))
+    classifier.add(Dropout(0.4))
+    classifier.add(BatchNormalization())
+    classifier.add(Dense(units = 1000, activation = 'relu'))
+    classifier.add(Dropout(0.2))
+    classifier.add(BatchNormalization())
+    classifier.add(Dense(units = 38, activation = 'softmax'))
+
+    classifier.load_weights('/kaggle/input/alexnet/tensorflow2/v1.0/1/AlexNetModel.hdf5')
+
+    return classifier
+
+def predict_image(model, image_path):
+    diseases = ['Apple scab',
+    'Apple Black rot',
+    'Apple Cedar_apple rust',
+    'Apple healthy',
+    'Blueberry healthy',
+    'Cherry Powdery_mildew',
+    'Cherry healthy',
+    'Maize Cercospora leaf spot',
+    'Maize Common rust',
+    'Maize Northern Leaf Blight',
+    'Maize healthy',
+    'Grape Black rot',
+    'Grape Esca Black Measles',
+    'Grape Leaf blight Isariopsis Leaf Spot',
+    'Grape healthy',
+    'Orange Haunglongbing Citrus greening',
+    'Peach Bacterial_spot',
+    'Peach healthy',
+    'Pepper bell Bacterial spot',
+    'Pepper bell healthy',
+    'Potato Early blight',
+    'Potato Late_blight',
+    'Potato healthy',
+    'Raspberry healthy',
+    'Soybean healthy',
+    'Squash Powdery mildew',
+    'Strawberry Leaf scorch',
+    'Strawberry healthy',
+    'Tomato Bacterial spot',
+    'Tomato Early blight',
+    'Tomato Leaf Mold',
+    'Tomato Septoria leaf spot',
+    'Tomato Spider mites Two-spotted spider mite',
+    'Tomato Target_Spot',
+    'Tomato_Yellow Leaf Curl Virus',
+    'Tomato mosaic virus',
+    'Tomato healthy'
+    ]
+    img = Image.open(image_path)
+    img = img.resize((224, 224))
+    img = np.array(img)
+    img = img / 255.0
+    img = np.expand_dims(img, axis=0)
+    pred = model.predict(img)
+    pred = tf.nn.softmax(pred, axis=1).numpy()
+    pred = pred[0]
+    pred = pred.tolist()
+    pred = {diseases[i]: pred[i] for i in range(len(diseases))}
+    class_max_prob = max(pred, key=pred.get)
+    
+    return class_max_prob
 
 app = FastAPI()
 
@@ -30,7 +141,7 @@ app.add_middleware(
     allow_headers=["*"], 
 )
 
-alexnet_model = utils.load_model_alexnet()
+alexnet_model = load_model_alexnet()
 tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen-VL-Chat", trust_remote_code=True) 
 llm_model = AutoModelForCausalLM.from_pretrained("sanjay-29-29/GreenAI", trust_remote_code=True, device_map='auto') 
 history = None
@@ -62,7 +173,7 @@ async def plant_image(query: str = Body(...), image: UploadFile = File(...)):
         return {"error": "Error in image processing"}
     
     query = extract_text_from_multipart(query)
-    op_text = utils.predict_image(alexnet_model, "image.jpg")
+    op_text = predict_image(alexnet_model, "image.jpg")
     op_text = op_text.lower()
     if('healthy' in op_text):
         response = "The plant is healthy"
@@ -82,5 +193,4 @@ async def plant_image(query: str = Body(...)):
     return {"response": response}
 
 if __name__ == "__main__":
-    resnet_model = utils.load_model_weights(utils.ResNet9(3,38), 'plant-disease-model-complete.pth')
     threading.Thread(target=run_server).start()
