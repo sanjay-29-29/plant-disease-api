d0a29fda40d1c3915719154c18b0da040d44579c
Sanjay <sanjaysusi29@gmail.com>
2024-03-21 18:07:16 +0530

    change cuda visibility

--- api.py
+++ api.py
@@ -25,7 +25,7 @@ from PIL import Image
 
 physical_devices = tf.config.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
-
+tf.config.experimental.set_memory_growth(physical_devices[1], True)
 def load_model_alexnet():
 
     classifier = Sequential()
